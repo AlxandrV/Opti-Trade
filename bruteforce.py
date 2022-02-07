90f2adc376b3bd647130290aed3ec49e266fbd97
AlxandrV <a.vigneaud@gmail.com>
2022-02-07 18:56:25 +0100

    Add - print all combiantions possible

--- bruteforce.py
+++ bruteforce.py
@@ -1,5 +1,7 @@
 import csv
+
 from itertools import combinations
+from prettytable import PrettyTable
 
 BUDGET = 500
 FILE_ACTION = 'actions.csv'
@@ -12,7 +14,7 @@ with open(FILE_ACTION, 'r') as file:
     actionsList = [action for action in csvFile]
 
 
-# Determine minimum occurence for budget
+# Determine minimum entry for budget
 actionsSorted = sorted(actionsList, key=lambda row: int(row[1]))
 minRes = []
 for actions in actionsSorted:
@@ -35,9 +37,12 @@ for i in range(len(minRes), len(actionsList)+1):
             # print(sum(actionsPrice))
             combsActions.append(comb)
 
-for comb in combsActions:
-    actionsPrice = []
-    for actions in comb:
-        actionsPrice.append(int(actions[1]))
-    print(sum(actionsPrice))
-
+# Print all combinations
+for i in range(1, len(combsActions)+1):
+    print("Combinaison possible n°" + str(i) + " :")
+    combsPrettyTable = PrettyTable(["Actions name", "Price (€)", "Rendment (%)"])
+    # actionsPrice = [actions for actions in comb]
+    for actions in combsActions[i-1]:
+        combsPrettyTable.add_row([elem for elem in actions])
+        # actionsPrice.append(int(actions[1]))
+    print(combsPrettyTable)
