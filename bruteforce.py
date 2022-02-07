18e2a198d566c9039c2df9fc61311268f85de490
AlxandrV <a.vigneaud@gmail.com>
2022-02-07 18:00:01 +0100

    Add - generate all combinations where sum of price equal to budget

--- bruteforce.py
+++ bruteforce.py
@@ -1,13 +1,43 @@
 import csv
+from itertools import combinations
 
 BUDGET = 500
 FILE_ACTION = 'actions.csv'
 
+# Convert file CSV to array
 with open(FILE_ACTION, 'r') as file:
     csvFile = csv.reader(file)
     header = next(csvFile)
 
-    actionList = [action for action in csvFile]
+    actionsList = [action for action in csvFile]
 
-# print(sum([int(price[1]) for price in actionList]))
+
+# Determine minimum occurence for budget
+actionsSorted = sorted(actionsList, key=lambda row: int(row[1]))
+minRes = []
+for actions in actionsSorted:
+    if sum(minRes) >= BUDGET:
+        break
+    else:
+        minRes.append(int(actions[1]))
+        if sum(minRes) > BUDGET:
+            minRes.pop(-1)
+
+
+# Generate all combinations of actions
+combsActions = []
+for i in range(len(minRes), len(actionsList)+1):
+    for comb in combinations(actionsList, i):
+        actionsPrice = []
+        for actions in comb:
+            actionsPrice.append(int(actions[1]))
+        if sum(actionsPrice) <= BUDGET:
+            # print(sum(actionsPrice))
+            combsActions.append(comb)
+
+for comb in combsActions:
+    actionsPrice = []
+    for actions in comb:
+        actionsPrice.append(int(actions[1]))
+    print(sum(actionsPrice))
 
