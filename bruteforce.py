6b44cbecb506a1f257fbf722365ae2fbd5080667
AlxandrV <a.vigneaud@gmail.com>
2022-02-14 16:22:40 +0100

    Add - generate the combinations where the budget is respected and add benefits for each actions

--- bruteforce.py
+++ bruteforce.py
@@ -12,45 +12,48 @@ with open(FILE_ACTION, 'r') as file:
     header = next(csvFile)
 
     actionsList = [action for action in csvFile]
-
+    for actions in actionsList:
+        actions.append(float(actions[1]) * (float(actions[2]) / 100))
 
 # Determine minimum entry for budget
-actionsSorted = sorted(actionsList, key=lambda row: int(row[1]))
-minRes = []
+actionsSorted = sorted(actionsList, key=lambda row: float(row[1]), reverse=True)
+combMin = []
 for actions in actionsSorted:
-    if sum(minRes) >= BUDGET:
+    if sum(combMin) >= BUDGET:
         break
     else:
-        minRes.append(int(actions[1]))
-        if sum(minRes) > BUDGET:
-            minRes.pop(-1)
-
+        combMin.append(float(actions[1]))
+        if sum(combMin) > BUDGET:
+            combMin.pop(-1)
 
 # Generate all combinations of actions
 combsActions = []
-for i in range(len(minRes), len(actionsList)+1):
-    for comb in combinations(actionsList, i):
-        actionsPrice = []
-        for actions in comb:
-            actionsPrice.append(int(actions[1]))
-        if sum(actionsPrice) <= BUDGET:
-            combsActions.append(comb)
+for i in range(len(combMin), len(actionsList)+1):
+    for comb in combinations(actionsSorted, i):
+        budgetComb = [float(actions[1]) for actions in comb]
+        nameComb = [actions[0] for actions in comb]
+        if sum(budgetComb) <= BUDGET:
+            i = []
+            for elementInit in actionsList:
+                if nameComb.count(elementInit[0]) == 0 and float(elementInit[1]) + sum(budgetComb) <= BUDGET:
+                    i.append(True)
+            if len(i) == 0:
+                # print(sum(budgetComb))
+                combsActions.append(comb)
+
+print('Combinaisons possibles : ' + str(len(combsActions)))
+
+for comb in combsActions:
+    benefComb = [float(actions[3]) for actions in comb]
+    print(sum(benefComb))
 
 # Print all combinations
-for i in range(1, len(combsActions)+1):
-    print("Combinaison possible n°" + str(i) + " :")
-    combsPrettyTable = PrettyTable(["Actions name", "Price (€)", "Profits (%)", "Profits in 2 years (€)"])
-    for actions in combsActions[i-1]:
-        if len(actions) == 4:
-            actions.pop(-1)
-        actions.append(round(int(actions[1]) / 100 * int(actions[2]), 2))
-        combsPrettyTable.add_row([elem for elem in actions])
-    # print(combsPrettyTable)
-
-benefs = []
-for combs in combsActions:
-    benef = [actions[3] for actions in combs]
-    benefs.append(max(benef))
-    # print(max(benef))
-maxValue = max(benefs)
-print([index for index, value in enumerate(benefs) if value == maxValue])
+# for i in range(1, len(combsActions)+1):
+#     print("Combinaison possible n°" + str(i) + " :")
+#     combsPrettyTable = PrettyTable(["Actions name", "Price (€)", "Profits (%)", "Profits in 2 years (€)"])
+#     for actions in combsActions[i-1]:
+#         if len(actions) == 4:
+#             actions.pop(-1)
+#         actions.append(round(int(actions[1]) / 100 * int(actions[2]), 2))
+#         combsPrettyTable.add_row([elem for elem in actions])
+#     # print(combsPrettyTable)
